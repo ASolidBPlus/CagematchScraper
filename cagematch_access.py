8fa22c90ff0a7090479d0587e3e9b74eae2abba1
ASolidBPlus <onethousandandfourholds@gmail.com>
2022-12-18 12:00:30 +1030

    Added error handling

--- cagematch_access.py
+++ cagematch_access.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from data_classes import wrestler, trainer
+from data_classes.wrestler import Wrestler
+from data_classes.trainer import Trainer
 
 class CagematchAccess:
     def __init__(self) -> None:
@@ -20,7 +21,7 @@ class CagematchWrestlerAccess(CagematchAccess):
         return self._construct_wrestler_data(cagematch_wrestler_id, wrestler_soup)
 
     def _construct_wrestler_data(self, cagematch_wrestler_id, wrestler_soup):
-        return wrestler.Wrestler(
+        return Wrestler(
             self._get_main_name(wrestler_soup),
             self._get_alter_egos(wrestler_soup),
             self._get_dob(cagematch_wrestler_id, wrestler_soup),
@@ -37,27 +38,54 @@ class CagematchWrestlerAccess(CagematchAccess):
             self._get_signature_moves(wrestler_soup))
 
     def _get_wrestler_data_base(self, wrestler_soup, text_search, return_text=True):
-        data = wrestler_soup.body.find(text=text_search).parent.find_next_sibling()
-        if return_text:
-            return data.text
 
-        return data
+        try:
+            data = wrestler_soup.body.find(text=text_search).parent.find_next_sibling()
+            if return_text:
+                return data.text
+
+            return data
+        
+        except:
+            pass
         
     def _get_split_entries(self, wrestler_soup, text_search):
-        return self._get_wrestler_data_base(wrestler_soup, text_search).split(", ")
+        try:
+            return self._get_wrestler_data_base(wrestler_soup, text_search).split(", ")
+        except:
+            return []
 
     def _get_br_entries(self, wrestler_soup, text_search):
         
-        br_data = self._get_wrestler_data_base(wrestler_soup, text_search, return_text=False).findAll('br')
-
         try:
-            return [br_data[0].previousSibling] + [br.nextSibling for br in br_data if br.nextSibling]
+            br_data = self._get_wrestler_data_base(wrestler_soup, text_search, return_text=False).findAll('br')
+
+            try:
+                return [br_data[0].previousSibling] + [br.nextSibling for br in br_data if br.nextSibling]
+
+            except:
+                try:
+                    return [self._get_wrestler_data_base(wrestler_soup, text_search)]
+
+                except:
+                    return []
         except:
-            return [self._get_wrestler_data_base(wrestler_soup, text_search)]
+            return []            
+
 
     def _get_id_from_url(self, link):
         match = re.search(r"nr=(\d+)", link)
         return match.group(1)
+
+    def _get_metric(self, metric_data, metric):
+        try:
+            match = re.search(fr"(\d+) {metric}", metric_data)
+            metric = int(match.group(1))
+            return(metric)
+
+        except:
+            return 0
+
 
     def _get_main_name(self, wrestler_soup):
         return wrestler_soup.find("h1").text
@@ -71,35 +99,37 @@ class CagematchWrestlerAccess(CagematchAccess):
     # TODO Re-use this method for weight too
     def _get_height(self, wrestler_soup):
         height_data = self._get_wrestler_data_base(wrestler_soup, "Height:")
-        match = re.search(r"(\d+) cm", height_data)
-        cm = int(match.group(1))
-        
-        return(cm)
+        return(self._get_metric(height_data, 'cm'))
+
 
     def _get_weight(self, wrestler_soup):
         weight_data = self._get_wrestler_data_base(wrestler_soup, "Weight:")
-        match = re.search(r"(\d+) kg", weight_data)
-        kg = int(match.group(1))
-        
-        return(kg)
+        return(self._get_metric(weight_data, 'kg'))
 
     def _get_background_in_sports(self, wrestler_soup):
         return self._get_split_entries(wrestler_soup, "Background in sports:")
 
     def _get_social_media_links(self, wrestler_soup):
         social_media_data = self._get_wrestler_data_base(wrestler_soup, "WWW:", return_text=False)
-        return [link['href'] for link in social_media_data.find_all('a', href=True)]
+
+        try:
+            return [link['href'] for link in social_media_data.find_all('a', href=True)]
+        except:
+            return []
 
     def _get_wrestling_style(self, wrestler_soup):
-        print("No it does not?")
         return self._get_split_entries(wrestler_soup, "Wrestling style:")
 
     def _get_alter_egos(self, wrestler_soup):
         alter_ego_data = self._get_wrestler_data_base(wrestler_soup, "Alter egos:", return_text=False)
-        return [alter_ego.text for alter_ego in alter_ego_data.find_all('a')]
+        return [alter_ego.text.strip() for alter_ego in alter_ego_data.find_all('a')]
 
     def _get_nicknames(self, wrestler_soup):
-        return [nickname.strip('"') for nickname in self._get_br_entries(wrestler_soup, "Nicknames:")]
+        brs = self._get_br_entries(wrestler_soup, "Nicknames:")
+        if brs: 
+            return [nickname.strip('"') for nickname in brs]
+        
+        return brs
 
     def _get_signature_moves(self, wrestler_soup):
         return self._get_br_entries(wrestler_soup, "Signature moves:")
@@ -123,18 +153,23 @@ class CagematchWrestlerAccess(CagematchAccess):
                 print(f"Warning: An issue was found when attempting to parse the date for id {cagematch_wrestler_id}, this was done during matching regex for the id on one of the search entries.")
     
     def _get_roles(self, wrestler_soup):
-        print("Does it fail here")
         return self._get_br_entries(wrestler_soup, "Roles:")
 
     def _get_trainers(self, wrestler_soup):
-        trainers_data = self._get_wrestler_data_base(wrestler_soup, "Trainer:", False).find_all('a', href=True)
         trainer_entries = []
 
-        for trainer_data in trainers_data:
-            trainer_entries.append(
-                trainer.Trainer(
-                trainer_data.text,
-                self._get_id_from_url(trainer_data['href'])
-            ))
+        try:
+            trainers_data = self._get_wrestler_data_base(wrestler_soup, "Trainer:", False).find_all('a', href=True)
 
-        return trainer_entries
+            for trainer_data in trainers_data:
+                trainer_entries.append(
+                    Trainer(
+                    trainer_data.text,
+                    self._get_id_from_url(trainer_data['href'])
+                ))
+
+            return trainer_entries
+
+        except:
+            return trainer_entries
+
