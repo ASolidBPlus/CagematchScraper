b836abae4d594f424951d5c05d54e7abdc66f710
ASolidBPlus <onethousandandfourholds@gmail.com>
2023-01-10 07:49:27 +1030

    Simplified coding logic in match.py, started implementing _get_matches function

--- cagematch/accessors/cagematch_event_accessor.py
+++ cagematch/accessors/cagematch_event_accessor.py
@@ -1,5 +1,6 @@
 import logging
 from cagematch.accessors.cagematch_accessor import CagematchAccessor
+import re
 
 
 class CagematchEventAccessor(CagematchAccessor):
@@ -23,6 +24,7 @@ class CagematchEventAccessor(CagematchAccessor):
         data['broadcast_type'] = cls._get_event_broadcast_type(soup)
         data['broadcast_date'] = cls._get_event_broadcast_date(soup)
         data['tv_station_network'] = cls._get_tv_station_network(soup)
+        data['matches'] = cls._get_matches(soup)
 
     @classmethod
     def _get_event_name(cls, soup):
@@ -59,3 +61,49 @@ class CagematchEventAccessor(CagematchAccessor):
     @classmethod
     def _get_tv_station_network(cls, soup):
         return cls._get_element_by_text_in_information_box(soup, "TV station/network:")
+
+    @classmethod
+    def _get_participants_data(cls, soup):
+        participants_soup = soup.find('div', class_='Caption', text='All workers').find_next_sibling()
+        
+        # Extract the HTML of the div
+        div_html = str(participants_soup)
+
+        # Create a regular expression to match the text and link of an anchor element
+        # The regular expression will match the opening and closing anchor tags, and capture the text and link
+        pattern = r'<a href="(?P<link>.*?)">(?P<text>.*?)</a>'
+
+        # Find all occurrences of the pattern in the HTML
+        matches = re.finditer(pattern, div_html)
+
+        # Create an empty list to store the text and links
+        elements_list = []
+
+        # Iterate through the matches
+        for match in matches:
+            # Extract the text and link from the match
+            text = match.group('text')
+            link = match.group('link')
+            elements_list.append({'text': text, 'link': link})  # Add the text and link to the list
+
+        # Extract the remaining text from the HTML
+        remaining_text = re.sub(pattern, '', div_html)
+
+        # Split the remaining text into a list, using the comma and space as the delimiter
+        remaining_text_list = remaining_text.split(', ')
+
+        # Remove any blank elements from the list
+        remaining_text_list = [{'text': x, 'link': None} for x in remaining_text_list if x]
+
+
+        # Add the remaining text items to the list
+        elements_list.extend(remaining_text_list)
+
+        return elements_list
+
+
+    @classmethod
+    def _get_matches(cls, soup):
+        match_soup = soup.find_all("div", class_="Match")
+        participant_data = cls._get_participants_data(soup)
+        print(participant_data)
--- cagematch/data_classes/match.py
+++ cagematch/data_classes/match.py
@@ -4,32 +4,48 @@ from cagematch.global_utils import pair
 
 @dataclass
 class BaseParticipant:
+    """A class representing a participant in a cagematch.
+
+    This class is designed to be extended by the SingleParticipant and TeamParticipant classes.
+    """
     name: str
+    """The name of the participant."""
     manager: list
+    """A list of managers for the participant."""
     notes: str
+    """Notes about the participant."""
     champion: bool
+    """A flag indicating whether the participant is a champion."""
 
     def __str__(self):
+        """Return a string representation of the participant.
+
+        This string will include the name, manager, champion status, and notes of the participant.
+        """
         formatted_data = [data for data in [self._format_name(), self._format_manager(), self._format_champion(), self._format_notes()] if data]
 
         return " ".join(formatted_data)
 
     def _format_name(self):
+        """Return the formatted name of the participant."""
         return self.name
 
     def _format_champion(self):
+        """Return the formatted champion status of the participant."""
         if self.champion:
             return "(c)"
         
         return None
 
     def _format_manager(self):
+        """Return the formatted manager of the participant."""
         if self.manager:
             return f"(w/ {pair(self.manager)})"
 
         return None
 
     def _format_notes(self):
+        """Return the formatted notes for the participant."""
         if self.notes:
             return f"[{self.notes}]"
 
@@ -38,64 +54,113 @@ class BaseParticipant:
 
 @dataclass
 class SingleParticipant(BaseParticipant, partial_cagematch_objects.PartialCagematchObject):
+    """A class representing a single participant in a cagematch.
+
+    This class extends the BaseParticipant class and implements the PartialCagematchObject interface.
+    """
 
     def get_full_object(self):
+        """Return the full object for this participant.
+
+        This method is required by the PartialCagematchObject interface.
+        """
         pass
 
 
 @dataclass
 class TeamParticipant(BaseParticipant, partial_cagematch_objects.PartialCagematchObject):
+    """A class representing a team of participants in a cagematch.
+
+    This class extends the BaseParticipant class and implements the PartialCagematchObject interface.
+    """
     members: list
+    """A list of members in the team."""
 
     def _format_name(self):
-        return f"{self.name} ({pair(self.members)})"
+        """Return the formatted name of the team.
+
+        If a name is provided, it will be used and the names of the team members will be appended in parentheses. 
+        Otherwise, the names of the team members will be used.
+        """
+        if self.name:
+            return f"{self.name} ({pair(self.members)})"
+        
+        return pair(self.members)
 
     def get_full_object(self):
+        """Return the full object for this team.
+
+        This method is required by the PartialCagematchObject interface.
+        """
         pass
 
 
 @dataclass
 class Match(cagematch_object.CagematchObject):
-    match_type: str
-    participant_blocks: list
-    match_notes: str
-    winning_participants: list
-    special_match_result: str
-    title_change: bool
+    """A class representing a match in a cagematch.
 
-    def _format_participant_blocks(self, join_str, participant_type = None):
+    This class extends the CagematchObject class and includes information about the type of match, participants, 
+    match notes, winning participants, special match result, and whether there was a title change.
+    """
+    match_type: str
+    """The type of match (e.g. singles, tag team, etc.)."""
+    participants: list
+    """A list of participants in the match."""
+    match_notes: str
+    """Notes about the match."""
+    winning_participants: list
+    """A list of the winning participants in the match."""
+    special_match_result: str
+    """A special result for the match (e.g. DQ, countout, etc.)."""
+    title_change: bool
+    """A flag indicating whether there was a title change in the match."""
+
+    def _format_participants(self, join_str, participant_type = None):
+        """Return a string of the formatted participants in the match.
+
+        Args:
+            join_str (str): The string to join the formatted participants with.
+            participant_type (str, optional): The type of participants to include. Can be 'losing', 'winning', or None 
+                (default) to include all participants.
+        
+        Returns:
+            str: A string of the formatted participants in the match.
+        """
         if participant_type == 'losing':
-            working_participants = [participant for participant in self.participant_blocks if participant not in self.winning_participants]
+            working_participants = [participant for participant in self.participants if participant not in self.winning_participants]
 
         elif participant_type == 'winning':
-            working_participants = [participant for participant in self.participant_blocks if participant in self.winning_participants]
-
+            working_participants = [participant for participant in self.participants if participant in self.winning_participants]
+  
         else:
-            working_participants = self.participant_blocks
-
-        formatted_participant_blocks = []
+            working_participants = self.participants
         
-        if len(working_participants) > 1:
-            for participant_block in working_participants:
-                formatted_participant_blocks.append(pair(participant_block))
-
-        elif len(working_participants) == 1 and len(self.participant_blocks) == 1:
-            formatted_participant_blocks += working_participants
-
-        elif len(working_participants) == 1 and len(self.participant_blocks) > 1:
-            formatted_participant_blocks.append(pair(working_participants))
-        
-        return join_str.join(str(formatted_participant) for formatted_participant in formatted_participant_blocks)
+        return join_str.join(str(working_participant) for working_participant in working_participants)
 
     def get_match_fixture(self):
-        return self._format_participant_blocks(" vs. ")
+        """Return a string of the match fixture.
+
+        The match fixture is a string representation of the participants in the match, separated by " vs. ".
+        
+        Returns:
+            str: A string of the match fixture.
+        """
+        return self._format_participants(" vs. ")
     
     def get_result_fixture(self):
+        """Return a string of the result fixture.
+
+        The result fixture is a string representation of the result of the match, including the match fixture, special 
+        match result (if applicable), and whether there was a title change (if applicable).
+        
+        Returns:
+            str: A string of the result fixture.
+        """
         if self.special_match_result in ['No Contest', 'Draw']:
             return f"{self.get_match_fixture()} - {self.special_match_result}"
 
-        formatted_losing_partitipants = self._format_participant_blocks(' and ', 'losing')
-        formatted_winning_participants = self._format_participant_blocks(' and ', 'winning')
+        formatted_losing_partitipants = self._format_participants(' and ', 'losing')
+        formatted_winning_participants = self._format_participants(' and ', 'winning')
 
         result = f"{formatted_winning_participants} defeats {formatted_losing_partitipants}"
                 
